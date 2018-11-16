8c9a09d91766cabf423fc1a329e98ba8e3fbf2d5
Raymond Chow <raychow1944@yahoo.com>
2018-11-16 06:23:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ function nowServing(line) {
   if(line.length === 0) return "There is nobody waiting to be served!";
   return "Currently serving " + line.shift() + ".";
 }
+
+function currentLine(line) {
+  if(line.length === 0) return "The line is currently empty."
+  var str = "The line is currently: ";
+  for(let i = 0; i < line.length; i++) {
+    
+  }
+}
