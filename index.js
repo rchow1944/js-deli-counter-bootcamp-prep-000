5b9b7607c8f05063548585917ce8f091232072c1
Raymond Chow <raychow1944@yahoo.com>
2018-11-16 06:25:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ function nowServing(line) {
 
 function currentLine(line) {
   if(line.length === 0) return "The line is currently empty."
-  var str = "The line is currently: ";
+  var str = `The line is currently:`;
   for(let i = 0; i < line.length; i++) {
-    
+    str += ` ${i+1} ${line[i]},`
   }
 }
