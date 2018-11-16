1e610d5ee70868262b517c6057ccf877b08c40c0
Raymond Chow <raychow1944@yahoo.com>
2018-11-16 06:15:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function takeANumber(line, name) {
   line.push(name);
   return `Welcome, ${name}. You are number ${line.length} in line.`;
 }
+
+function nowServing(line) {
+  if(line.length === 0) return "There is nobody waiting to be served!";
+  return line.shift();
+}
